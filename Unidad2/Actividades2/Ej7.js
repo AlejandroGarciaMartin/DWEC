4ef6fc2cb2b80642328411df895cd574515f41b3
Alejandro Garcia <alegarmar10@alu.edu.gva.es>
2023-09-27 17:08:16 +0200

    ejercicios hasta el 7 hechos

---
+++ Unidad2/Actividades2/Ej7.js
@@ -0,0 +1,12 @@
+let fin;
+let operacion
+let i;
+let ask 
+
+while(fin == 0){
+
+    ask = parseInt(prompt('Indica numero, cuando no quieras mas introduce un 0 para acabar.'));
+    operacion = operacion + ask
+    i++;
+
+}
