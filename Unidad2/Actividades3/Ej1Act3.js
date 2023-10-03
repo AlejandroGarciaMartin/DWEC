c46be20bde47d7e997d1544d075e753ac2cd0db2
Alejandro Garcia <alegarmar10@alu.edu.gva.es>
2023-10-03 17:29:50 +0200

    ejercicios hasta el 1 hechos de actividades3

---
+++ Unidad2/Actividades3/Ej1Act3.js
@@ -0,0 +1,6 @@
+
+let x = Math.floor(Math.random() * 256);
+let y = Math.floor(Math.random() * 256);
+let z = Math.floor(Math.random() * 256);
+
+document.body.style.backgroundColor = `rgb(${x}, ${y}, ${z})`;
